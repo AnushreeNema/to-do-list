2734b6180b105cd5dc26de63494aa4a95879411c
Anushree Nema <anushreenema624@gmail.com>
2025-07-04 19:50:49 +0530

    testing js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let myvariable = 345;
   return (
     <div className="App">
       <header className="App-header">
+        <div>{myvariable}</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
